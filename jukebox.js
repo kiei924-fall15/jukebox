81e79fb076f9c60d811e4b2060685ad074425a65
Brian Eng <brian@brianeng.com>
2015-11-13 10:31:05 -0600

    cleanup

--- jukebox.js
+++ jukebox.js
@@ -1,20 +1,23 @@
 $(function() {
-  var fetchTracks = function (albumId, callback) {
+  var fetchTracks = function(albumId, callback) {
     $.ajax({
       url: 'https://api.spotify.com/v1/albums/' + albumId,
-      success: function (response) {
+      success: function(response) {
         callback(response);
       }
     });
   };
-  var searchAlbums = function (query) {
+  var searchAlbums = function(query) {
     $.ajax({
       url: 'https://api.spotify.com/v1/search',
       data: {
-          q: query,
-          type: 'album'
+        q: query,
+        type: 'album'
       },
-      success: function (response) {
+      success: function(response) {
+        // Set a window-level variable so we can use it in the console
+        window.response = response;
+
         // Write the response from Spotify to the console, in pretty collapsible way
         console.log(response);
 
@@ -44,5 +47,11 @@ $(function() {
       window.audioObject = new Audio(data.tracks.items[0].preview_url);
       window.audioObject.play();
     });
+  });
+  $(document).on('click', '.stop-it', function(event) {
+    event.preventDefault();
+    if (window.audioObject !== undefined) {
+      window.audioObject.pause();
+    }
   });
 });
